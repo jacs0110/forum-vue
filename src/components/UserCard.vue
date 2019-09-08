934ed098cf8d8cf849fcf1fc27ec199b91c12ad9
jacs <wbruce0110@gmail.com>
2019-09-08 15:11:16 +0800

    create UserCard component and update UserTop page

---
+++ src/components/UserCard.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="col-4">
+    <a href="#">
+      <img :src="user.image" width="140px" height="140px" />
+    </a>
+    <h2>{{user.name}}</h2>
+    <span class="badge badge-secondary">Followers：{{user.FollowerCount}}</span>
+    <p class="mt-3">
+      <button
+        v-if="user.isFollowed"
+        type="button"
+        class="btn btn-sm btn-danger mr-2"
+        @click.stop.prevent="unfollow"
+      >Unfollow</button>
+      <button
+        v-else
+        type="button"
+        class="btn btn-sm btn-primary mr-2"
+        @click.stop.prevent="follow"
+      >Follow</button>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    // eslint-disable-next-line
+    user: {
+      type: Array,
+      required: true
+    }
+  },
+  data() {
+    return {
+      // eslint-disable-next-line
+      user: this.user
+    };
+  },
+  methods: {
+    follow() {
+      this.user = {
+        ...this.user,
+        isFollowed: true
+      };
+    },
+    unfollow() {
+      this.user = {
+        ...this.user,
+        isFollowed: false
+      };
+    }
+  }
+};
+</script>
--- src/views/UsersTop.vue
+++ src/views/UsersTop.vue
@@ -2,14 +2,113 @@
   <div class="container py-5">
     <NavTabs />
     <h1 class="mt-5">Top Users</h1>
+    <hr />
+    <div class="row text-center">
+      <UserCard v-for="user in users" :key="user.id" :user="user" />
+    </div>
   </div>
 </template>
 
 <script>
 import NavTabs from "./../components/NavTabs";
+import UserCard from "./../components/UserCard";
+
+const dummyData = {
+  users: [
+    {
+      id: 23,
+      name: "root-test",
+      email: "root@example.com",
+      password: "$2a$10$P2kn1Pnu2TIEtniHEK.vr.J1.oxzADZ8koKTgrODqK8Cy2v0l.om6",
+      isAdmin: true,
+      image: "https://i.imgur.com/wuBpQcD.jpg",
+      createdAt: "2019-08-30T10:04:58.000Z",
+      updatedAt: "2019-09-03T15:47:54.000Z",
+      Followers: [
+        {
+          id: 24,
+          name: "user1",
+          email: "user1@example.com",
+          password:
+            "$2a$10$Y3ACAUofMHsf1S.R.qUYM.7nrm9Pt4Y/o.TuryJ4pJL4O6cdHL3yC",
+          isAdmin: false,
+          image: null,
+          createdAt: "2019-08-30T10:04:58.000Z",
+          updatedAt: "2019-09-03T15:49:08.000Z",
+          Followership: {
+            followerId: 24,
+            followingId: 23,
+            createdAt: "2019-08-31T00:55:56.000Z",
+            updatedAt: "2019-08-31T00:55:56.000Z"
+          }
+        },
+        {
+          id: 23,
+          name: "root-test",
+          email: "root@example.com",
+          password:
+            "$2a$10$P2kn1Pnu2TIEtniHEK.vr.J1.oxzADZ8koKTgrODqK8Cy2v0l.om6",
+          isAdmin: true,
+          image: "https://i.imgur.com/wuBpQcD.jpg",
+          createdAt: "2019-08-30T10:04:58.000Z",
+          updatedAt: "2019-09-03T15:47:54.000Z",
+          Followership: {
+            followerId: 23,
+            followingId: 23,
+            createdAt: "2019-09-03T15:54:52.000Z",
+            updatedAt: "2019-09-03T15:54:52.000Z"
+          }
+        }
+      ],
+      FollowerCount: 2,
+      isFollowed: true
+    },
+    {
+      id: 24,
+      name: "user1",
+      email: "user1@example.com",
+      password: "$2a$10$Y3ACAUofMHsf1S.R.qUYM.7nrm9Pt4Y/o.TuryJ4pJL4O6cdHL3yC",
+      isAdmin: false,
+      image: null,
+      createdAt: "2019-08-30T10:04:58.000Z",
+      updatedAt: "2019-09-03T15:49:08.000Z",
+      Followers: [],
+      FollowerCount: 0,
+      isFollowed: false
+    },
+    {
+      id: 25,
+      name: "user2",
+      email: "user2@example.com",
+      password: "$2a$10$1pWkFo.EWbKX00gHF6qnNuGxCG4QfMYUrNRFCkjlBqcWFTxdP.jk6",
+      isAdmin: false,
+      image: null,
+      createdAt: "2019-08-30T10:04:58.000Z",
+      updatedAt: "2019-08-30T10:04:58.000Z",
+      Followers: [],
+      FollowerCount: 0,
+      isFollowed: false
+    }
+  ]
+};
+
 export default {
   components: {
-    NavTabs
+    NavTabs,
+    UserCard
+  },
+  data() {
+    return {
+      users: []
+    };
+  },
+  created() {
+    this.fetchUsers();
+  },
+  methods: {
+    fetchUsers() {
+      this.users = dummyData.users;
+    }
   }
 };
 </script>
